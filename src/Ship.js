374a571f44eed32e5c7dec35bcf8c2200965639c
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2025-12-19 22:55:14 +0900

    Conceal length and hits as private

--- src/Ship.js
+++ src/Ship.js
@@ -1,15 +1,19 @@
 class Ship {
+  #length;
+
+  #hits;
+  
   constructor(length) {
-    this.length = length;
-    this.hits = 0;
+    this.#length = length;
+    this.#hits = 0;
   }
 
   hit() {
-    this.hits += 1;
+    this.#hits += 1;
   }
 
   isSunk() {
-    return this.hits === this.length;
+    return this.#hits === this.#length;
   }
 }
 
