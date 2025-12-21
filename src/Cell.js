db7d9e53e85e73bfb794bc2ff4ff44581c758447
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2025-12-21 23:24:32 +0900

    Add isOccupied and correlated methods to Cell.js
    Need to mark adjacent cells as occupied when a ship is placed.

--- src/Cell.js
+++ src/Cell.js
@@ -3,20 +3,32 @@ class Cell {
   
   #isAttacked;
 
+  #isOccupied;
+
   constructor() {
-    this.#isAttacked = false;
     this.#ship = null;
+    this.#isAttacked = false;
+    this.#isOccupied = false;
   }
 
   isAttacked() {
     return this.#isAttacked;
   }
 
+  
   getAttacked() {
     if (this.#isAttacked) return;
-
+    
     this.#isAttacked = true;
     this.#ship?.hit();
+  }
+  
+  isOccupied() {
+    return this.#isOccupied;
+  }
+
+  becomesOccupied() {
+    this.#isOccupied = true;
   }
   
   getShip() {
