e28c0a8cbe420fa45078d8d234afb2bfd590eab3
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2025-12-24 22:32:10 +0900

    Handle exception for invalid coordinate and length

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,3 +1,4 @@
+import AXIS from './AXIS';
 import Cell from './Cell';
 
 class Gameboard {
@@ -17,6 +18,18 @@ class Gameboard {
 
   getCellAt(x, y) {
     return this.#board[x][y];
+  }
+
+  placeShip(x, y, axis, length) {
+    if (!(x >= 0 && x < 10) || !(y >= 0 && y < 10)) {
+      throw Error('Invalid coordinate. x and y should be between 0 and 9.');
+    }
+
+    if (!(length >= 1 && length <= 4)) {
+      throw Error('Invalid length. The length should be between 1 and 4.');
+    }
+
+    
   }
 }
 
