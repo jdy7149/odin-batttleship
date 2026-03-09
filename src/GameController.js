4fa788d34b9fb0328d70ed6d0a816db9d18b3643
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2026-03-09 19:03:04 +0900

    End GameController

--- src/GameController.js
+++ src/GameController.js
@@ -1,61 +1,79 @@
-import Player from './Player'
-import AXIS from './AXIS';
+import Player from './Player';
 
 class GameController {
   #user;
-
   #computer;
-  
-  #activePlayer;
-
   #isEnd;
 
   constructor() {
     this.#user = new Player();
     this.#computer = new Player();
-    this.#activePlayer = this.#user;
     this.#isEnd = false;
 
     // Place ships for both players at predetermined coordinates
     [this.#user, this.#computer].forEach((player) => {
       const gameboard = player.getGameboard();
 
-      gameboard.placeShip(0, 1, AXIS.HORIZONTAL, 1);
-      gameboard.placeShip(2, 2, AXIS.HORIZONTAL, 1);
-      gameboard.placeShip(3, 9, AXIS.HORIZONTAL, 1);
-      gameboard.placeShip(9, 3, AXIS.HORIZONTAL, 1);
-      gameboard.placeShip(2, 5, AXIS.HORIZONTAL, 2);
-      gameboard.placeShip(6, 1, AXIS.VERTICAL, 2);
-      gameboard.placeShip(8, 9, AXIS.VERTICAL, 2);
-      gameboard.placeShip(0, 6, AXIS.HORIZONTAL, 3);
-      gameboard.placeShip(7, 7, AXIS.VERTICAL, 3);
-      gameboard.placeShip(4, 5, AXIS.VERTICAL, 3);
+      gameboard.placeShip(0, 0, 1, 1);
+      gameboard.placeShip(2, 2, 2, 2);
+      gameboard.placeShip(3, 3, 9, 9);
+      gameboard.placeShip(9, 9, 3, 3);
+      gameboard.placeShip(2, 2, 5, 6);
+      gameboard.placeShip(6, 7, 1, 1);
+      gameboard.placeShip(8, 9, 9, 9);
+      gameboard.placeShip(0, 0, 6, 8);
+      gameboard.placeShip(7, 9, 7, 7);
+      gameboard.placeShip(4, 7, 5, 5);
     });
   }
 
-  getActivePlayer() {
-    return this.#activePlayer;
-  }
+  attackComputer(x, y) {
+    const comGameboard = this.#computer.getGameboard();
 
-  playRound(x, y) {
-    if (this.#isEnd) return;
+    const result = comGameboard.receiveAttack(x, y);
 
-    const inactivePlayer = this.#activePlayer === this.#user ? this.#computer : this.#user;
-
-    const gameboard = inactivePlayer.getGameboard();
-
-    gameboard.receiveAttack(x, y);
-
-    if (gameboard.isEliminated()) {
+    if (comGameboard.isEliminated()) {
       this.#isEnd = true;
-      return;
     }
-    
-    this.#switchPlayerTurn();
+
+    return result;
   }
 
-  #switchPlayerTurn() {
-    this.#activePlayer = this.#activePlayer === this.#user ? this.#computer : this.#user;
+  randomAttackUser() {
+    const userGameboard = this.#user.getGameboard();
+
+    while (true) {
+      const [x, y] = [
+        Math.floor(Math.random() * 10),
+        Math.floor(Math.random() * 10),
+      ];
+
+      const randomCell = userGameboard.getCellAt(x, y);
+
+      if (randomCell.isAttacked()) {
+        continue;
+      }
+
+      randomCell.attack();
+
+      if (userGameboard.isEliminated()) {
+        this.#isEnd = true;
+      }
+
+      return randomCell.getState();
+    }
+  }
+
+  isEnd() {
+    return this.#isEnd;
+  }
+
+  getUser() {
+    return this.#user;
+  }
+
+  getComputer() {
+    return this.#computer;
   }
 }
 
