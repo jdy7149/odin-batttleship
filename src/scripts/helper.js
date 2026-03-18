c41a58712751c97f9164e0f2b952e2e1e700f36e
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2026-03-18 19:33:41 +0900

    Create helper.js which contains helper func

---
+++ src/scripts/helper.js
@@ -0,0 +1,11 @@
+const shuffleArray = (arr) => {
+  for (let i = this.arr.length - 1; i > 0; i--) {
+    const randomidx = Math.floor(Math.random() * (i + 1));
+
+    [this.arr[i], this.arr[randomidx]] = [this.arr[randomidx], this.arr[i]];
+  }
+}
+
+const isValidCoordinate = (k) => k >= 0 && k < 10;
+
+export { shuffleArray, isValidCoordinate }
