d39ae5149cf72ff247875edfb3ccd059e4d6fe54
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2026-03-18 21:25:16 +0900

    Add randomlyPlaceShips

--- src/scripts/GameController.js
+++ src/scripts/GameController.js
@@ -1,36 +1,53 @@
+import AXIS from './AXIS';
+import CELL_STATE from './CELL_STATE';
 import Player from './Player';
+
+import { shuffleArray } from './helper';
 
 class GameController {
   #user;
   #computer;
+  #isUserTurn;
   #isEnd;
+  #randomCoords;
 
   constructor() {
     this.#user = new Player();
     this.#computer = new Player();
+    this.#isUserTurn = true;
     this.#isEnd = false;
 
-    // Place ships for both players at predetermined coordinates
-    [this.#user, this.#computer].forEach((player) => {
-      const gameboard = player.getGameboard();
+    // Random coords which computer will attack on user's gameboard
+    this.#randomCoords = [];
 
-      gameboard.placeShip(0, 0, 1, 1);
-      gameboard.placeShip(2, 2, 2, 2);
-      gameboard.placeShip(3, 3, 9, 9);
-      gameboard.placeShip(9, 9, 3, 3);
-      gameboard.placeShip(2, 2, 5, 6);
-      gameboard.placeShip(6, 7, 1, 1);
-      gameboard.placeShip(8, 9, 9, 9);
-      gameboard.placeShip(0, 0, 6, 8);
-      gameboard.placeShip(7, 9, 7, 7);
-      gameboard.placeShip(4, 7, 5, 5);
-    });
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        this.#randomCoords.push([i, j]);
+      }
+    }
+
+    shuffleArray(this.#randomCoords);
+
+    // Place computer's ships
+    this.randomlyPlaceShips('computer');
   }
 
   attackComputer(x, y) {
+    if (this.#isEnd) {
+      throw Error('Game has ended.');
+    }
+
     const comGameboard = this.#computer.getGameboard();
 
+    if (comGameboard.getCellAt(x, y).isAttacked()) {
+      return null;
+    }
+
     const result = comGameboard.receiveAttack(x, y);
+
+    if (result === CELL_STATE.MISSED) {
+      this.#isUserTurn = false;
+    }
 
     if (comGameboard.isEliminated()) {
       this.#isEnd = true;
@@ -40,27 +57,66 @@ class GameController {
   }
 
   randomAttackUser() {
+    if (this.#isEnd) {
+      throw Error('Game has ended.');
+    }
+
     const userGameboard = this.#user.getGameboard();
 
-    while (true) {
+    const [x, y] = this.#randomCoords.pop();
+
+    const result = userGameboard.receiveAttack(x, y);
+
+    if (result === CELL_STATE.MISSED) {
+      this.#isUserTurn = true;
+    }
+
+    if (userGameboard.isEliminated()) {
+      this.#isEnd = true;
+      this.#isUserTurn = true;
+    }
+
+    return result;
+  }
+
+  randomlyPlaceShips(target) {
+    let gameboard = null;
+
+    if (target === 'user') {
+      gameboard = this.#user.getGameboard();
+    } else if (target === 'computer') {
+      gameboard = this.#computer.getGameboard();
+    } else {
+      return;
+    }
+
+    // Place ships on random spots
+    const shipCounts = [0, 4, 3, 2, 1];
+    let attempts = 2000;
+
+    while (shipCounts.some((cnt) => cnt > 0) && attempts > 0) {
+      attempts -= 1;
+
+      const axis = Math.random() < 0.5 ? AXIS.HORIZONTAL : AXIS.VERTICAL;
+
+      const length = Math.floor(Math.random() * 4) + 1;
+
+      if (shipCounts[length] === 0) {
+        continue;
+      }
+
       const [x, y] = [
         Math.floor(Math.random() * 10),
         Math.floor(Math.random() * 10),
       ];
 
-      const randomCell = userGameboard.getCellAt(x, y);
-
-      if (randomCell.isAttacked()) {
+      try {
+        gameboard.placeShip(x, y, axis, length);
+      } catch (e) {
         continue;
       }
 
-      randomCell.attack();
-
-      if (userGameboard.isEliminated()) {
-        this.#isEnd = true;
-      }
-
-      return randomCell.getState();
+      shipCounts[length] -= 1;
     }
   }
 
@@ -68,12 +124,16 @@ class GameController {
     return this.#isEnd;
   }
 
-  getUser() {
-    return this.#user;
+  isUserTurn() {
+    return this.#isUserTurn;
   }
 
-  getComputer() {
-    return this.#computer;
+  getUserBoard() {
+    return this.#user.getGameboard();
+  }
+
+  getComputerBoard() {
+    return this.#computer.getGameboard();
   }
 }
 
